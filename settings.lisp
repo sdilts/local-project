d1b0b55436c79b9766162ad655eaafb2b13eeb04
sdilts <stuart.dilts@gmail.com>
2018-09-27 20:27:51 -0600

    Add some docs to settings.lisp

--- settings.lisp
+++ settings.lisp
@@ -4,13 +4,14 @@
 
 (in-package #:gdep/settings)
 
-(export '(data-dir-changed
-	  new-directory
-	  *data-directory*
-	  change-data-directory))
+(export '(*data-directory*))
 
 ;; TODO: change this directory:
 (defvar *data-directory* (uiop:ensure-directory-pathname #p"/home/stuart/Programs/git-install/data"))
 
 ;; TODO: change this:
-(defvar *pkg-database* "rpm")
+(defvar *pkg-database* "rpm"
+  "The system database type")
+
+(defvar *pkg-manager* "zypper"
+  "The system package manager")
