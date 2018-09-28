8b14077a09d946dd35c4ae6d0512c1f73cbd5622
sdilts <stuart.dilts@gmail.com>
2018-09-27 22:52:13 -0600

    Add command scan-scripts
    + Allows the addtion of new scripts without removing the database directory

--- data-init.lisp
+++ data-init.lisp
@@ -1,6 +1,8 @@
 ;;(in-package #:gdep/data-dir)
 (defpackage #:gdep/data-init
   (:use #:cl #:alexandria #:gdep/util)
+  (:import-from #:gdep/command
+		#:defcommand)
   (:import-from #:gdep/settings
 		:*data-directory*)
   (:import-from #:lambdalite
@@ -13,6 +15,19 @@
 (in-package #:gdep/data-init)
 
 (export '(data-dir-init))
+
+(defun scan-scripts ()
+  (scan-build-types *data-directory*)
+  (scan-version-control *data-directory*))
+
+(defcommand scan-scripts-cmd (current-directory command-line-args)
+    ("Scan the data directory for more scripts."
+     :command-used "scan-scripts"
+     :more-info '("Usage: gdep scan-scripts"
+		 " See the README in the data directory for more information"
+		  " about gdep scripts."))
+  (declare (ignore current-directory command-line-args))
+  (scan-scripts))
 
 (defun data-dir-init ()
   ;; make sure the directory is present:
@@ -33,6 +48,5 @@
 								      *data-directory*))))
     (when db-created
       (format *debug-io* "~&Initializing database...~%")
-      (scan-build-types *data-directory*)
-      (scan-version-control *data-directory*)
+      (scan-scripts)
       (format *debug-io* "~&Database initialized~%~%"))))
